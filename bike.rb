b36c5af4346e3068d35a96371f9125da9e361746
Ashley Doerfler <doerflas@oregonstate.edu>
2023-11-19 17:57:44 -0800

    Update bike.rb

--- bike.rb
+++ bike.rb
@@ -1,13 +1,12 @@
-class Rent_Bike
+class Cargo_Items
 
   MAX_CARGO_ITEMS = 10
 
-  attr_accessor :id, :rented_bikes, :avaliable_bikes, :cargo_contents
+  attr_accessor :id, :bike, :cargo_contents
 
-  def initialize(id, rented_bikes=[], avaliable_bikes=[])
+  def initialize(id, bike)
     @id = id
-    @rented_bikes = rented_bikes
-    @avaliable_bikes = avaliable_bikes
+    @bike = bike
     @cargo_contents = []
 
   def add_avaliable_bike(bike)
@@ -16,6 +15,7 @@ class Rent_Bike
   def rent_bike(bike)
     @avaliable_bikes.delete(bike)
     @rented_bikes << bike
+    add_cargo(bike)
 
   def return_bike(bike)
     @rented_bikes.delete(bike)
@@ -38,69 +38,14 @@ end
 class Bike
 
   STANDARD_WEIGHT = 200 # lbs
-  attr_accessor :id, :color, :price, :weight
+  attr_accessor :id, :color, :price, :weight, :rented
 
-  def initialize(id, color, price, weight = STANDARD_WEIGHT)
+  def initialize(id, color, price, weight = STANDARD_WEIGHT, rented=false)
     @id = id
     @color = color
     @price = price
     @weight = weight
+    @rented = rented
 
-  def rent!
-    self.rented = true
-
-  def add_cargo(item)
-    self.cargo_contents << item
-
-  def remove_cargo(item)
-    self.cargo_contents.remove(item)
-
-  def pannier_capacity
-    MAX_CARGO_ITEMS
-
-  def pannier_remaining_capacity
-    MAX_CARGO_ITEMS - self.cargo_contents.size
-
-end
-
-class Rent_Bike
-
-  def initialize(avaliable_bikes=[])
-    @avaliable_bikes = avaliable_bikes
-    @cargo_contents = []
-
-  def add_avaliable_bike(bike)
-    @avaliable_bikes << bike
-
-  def rent_bike(bike)
-    @avaliable_bikes.delete(bike)
-    @rented_bikes << bike
-
-  def return_bike(bike)
-    @rented_bikes.delete(bike)
-    @avaliable_bikes << bike
-
-  def add_cargo(item)
-    self.cargo_contents << item
-
-  def remove_cargo(item)
-    self.cargo_contents.remove(item)
-
-  def pannier_capacity
-    MAX_CARGO_ITEMS
-
-  def pannier_remaining_capacity
-    MAX_CARGO_ITEMS - self.cargo_contents.size
-
-end
-
-class Bike
-
-  def initialize(id, color, price, weight = STANDARD_WEIGHT)
-    @id = id
-    @color = color
-    @price = price
-    @weight = weight
-
-  def change_price(price)
-    @price = price
+  def rent_bike
+    @rented = true
